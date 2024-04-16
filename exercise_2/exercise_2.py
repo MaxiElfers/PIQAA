43cc43c4f11e9452c4164a2f0745292e8abe2818
luening <he-luening@t-online.de>
2024-04-16 09:23:02 +0200

    error handling and comments added

--- exercise_2/exercise_2.py
+++ exercise_2/exercise_2.py
@@ -1,6 +1,9 @@
+# Given an integer count of a number of donuts, return a string
+# of the form 'Number of donuts: <count>', where <count> is the number
+# passed in. However, if the count is 10 or more, then use the word 'many'
+# instead of the actual count.
 def donuts(count):
     try:
-        type(count) == int
         if count < 10:
             return 'Number of donuts: ' + str(count)
         else:
@@ -9,24 +12,43 @@ def donuts(count):
         return 'Not a valid number'
 
 
+# Given a string, if its length is at least 3, add 'ing' to its end.
+# Unless it already ends in 'ing', in which case add 'ly' instead.
+# If the string length is less than 3, leave it unchanged.
 def verbing(s):
-    # error handling
-    if type(s) != str:
-        print('Input must be a string')
-        return
+    try:
+        if len(s) >= 3:
+            # test if last 3 characters are equal to 'ing'
+            if s[-3:] == 'ing':
+                s = s.replace('ing', 'ly')
+            else:
+                s += 'ing'  
+        return s
+    except:
+        return 'Input must be a string'
     
-    if len(s) >= 3:
-        if s[-3:] == 'ing':
-            s += 'ly'
-        else:
-            s += 'ing'  
-    return s
+    
 
+# Given a list of numbers, return a list where all adjacent == elements have been reduced to a single element,
+# so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or modify the passed in list.
 def remove_adjacent(nums):
-    if len(nums) == 0:
-        return 'Empty Array provided'
-    else:
-        return
+    try:
+        if len(nums) != 0:
+            newList = []
+            # set currentElement to a unique value that is not a number
+            currentElement = ''
+            for element in nums:
+                # if earlier checked element unequal to new, append and set
+                if element != currentElement:
+                    newList.append(element)
+                    currentElement = element
+            return newList
+        else:
+            return 'Empty Array provided'
+    except:
+        return 'error'
+        
+        
 
 def main():
     print('donuts')
