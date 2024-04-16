a72cdec4513a103f0a952c13edb4da70bb520826
luening <he-luening@t-online.de>
2024-04-16 08:44:16 +0200

    added 2nd function in exercice 2

--- exercise_2/exercise_2.py
+++ exercise_2/exercise_2.py
@@ -2,9 +2,25 @@ def donuts(count):
     # +++your code here+++
     return
 
+# verbing
+# Given a string, if its length is at least 3,
+# add 'ing' to its end.
+# Unless it already ends in 'ing', in which case
+# add 'ly' instead.
+# If the string length is less than 3, leave it unchanged.
+# Return the resulting string.
 def verbing(s):
-    # +++your code here+++
-    return
+    # error handling
+    if type(s) != str:
+        print('Input must be a string')
+        return
+    
+    if len(s) >= 3:
+        if s[-3:] == 'ing':
+            s += 'ly'
+        else:
+            s += 'ing'  
+    return s
 
 def remove_adjacent(nums):
     # +++your code here+++
