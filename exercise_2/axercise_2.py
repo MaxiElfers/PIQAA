bf3a806bc9f3748ab711e80ec2bcb0428f2c0613
MaxiElfers <maximilian-elfers@gmx.de>
2024-04-16 08:30:26 +0200

    Template

---
+++ exercise_2/axercise_2.py
@@ -0,0 +1,33 @@
+def donuts(count):
+    # +++your code here+++
+    return
+
+def verbing(s):
+    # +++your code here+++
+    return
+
+def remove_adjacent(nums):
+    # +++your code here+++
+    return
+
+def main():
+    print('donuts')
+
+    print(donuts(4))
+    print(donuts(9))
+    print(donuts(10))
+    print(donuts('twentyone'))
+
+    print('verbing')
+    print(verbing('hail'))
+    print(verbing('swiming'))
+    print(verbing('do'))
+    print('remove_adjacent')
+
+    print(remove_adjacent([1, 2, 2, 3]))
+    print(remove_adjacent([2, 2, 3, 3, 3]))
+    print(remove_adjacent([]))
+    
+# Standard boilerplate to call the main() function.
+if __name__ == '__main__':
+    main()
