891a28f2a3fe95ce7c1f5a4a1d664d3d3f12faf5
Maximilian Elfers <91053215+MaxiElfers@users.noreply.github.com>
2024-06-11 20:22:38 +0200

    Fixed error

--- exercise_9/exercise_9_1.py
+++ exercise_9/exercise_9_1.py
@@ -11,13 +11,13 @@ fc_list = arcpy.ListFeatureClasses(feature_type='Point')
 fc_list.remove("active_assets")
 
 fc_path_insert = os.path.join(arcpy.env.workspace,"active_assets") # path for insert cursor
-cursor_fields = ['Shape@XY','status','type'] # Set all used fields
-icur = arcpy.da.InsertCursor(in_table=fc_path_insert,field_names=cursor_fields) # Create Insert cursor
 
 # Iterate over each item of the list
 for feat in fc_list:
     fc_path_search = os.path.join(arcpy.env.workspace,feat) # path for search cursor
-    
+
+    cursor_fields = ['Shape@XY','status','type'] # Set all used fields
+    icur = arcpy.da.InsertCursor(in_table=fc_path_insert,field_names=cursor_fields) # Create Insert cursor
     scur = arcpy.da.SearchCursor(in_table=fc_path_search,field_names=cursor_fields) # Create search cursor
 
     for row in scur:
